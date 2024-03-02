f02417851035ab49f6aa7fb3d9c8e16f8740568f
emiraelDev <emmzex19@gmail.com>
2024-03-02 16:34:01 +0100

    refomatted welcome and home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Welcome from "./pages/Welcome";
 
 function App() {
   return (
     <div className=" m-auto w-full md:w-[70%] lg:w-[50%] text-center text-gray-100 font-roboto">
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Welcome />} />
         </Routes>
       </Router>
     </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,32 +1,9 @@
-import { Link, useNavigate } from "react-router-dom";
 import NavBar from "../components/NavBar";
-import Welcome from "./Welcome";
-import { signInWithPopup } from "firebase/auth";
-import { auth, provider } from "../configurations/firebase";
 export default function Home() {
-  const navigate = useNavigate();
-  const signUserIn = async () => {
-    await signInWithPopup(auth, provider);
-    navigate("/");
-  };
   return (
     <div className="">
       <NavBar />
-      <div className="mt-32 ">
-        <div>
-          <p className=" text-lg italic text-center px-5 my-5 md:text-2xl">
-            Welcome to el-thoughts, share your opinion and connect with people
-          </p>
-
-          <Link
-            to="/signIn"
-            className="bg-green-500 w-32 px-10 rounded-md text-lg py-2"
-          >
-            sign in
-          </Link>
-        </div>
-        <Welcome />
-      </div>
+      <div>Home</div>
     </div>
   );
 }
